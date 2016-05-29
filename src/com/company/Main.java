b764a22f8afe222ef9c60c4cd317d6d43321fc3b
Deop <feukorv@gmail.com>
2016-05-29 10:54:08 +0300

    added read from file method, reading table header and 10 rows

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public static void readTestCasesFromFile(String filePath) throws IOException {
+        FileInputStream fs = new FileInputStream(filePath);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+        for (int i = 0; i < 11; i++) {
+            //br.readLine();
+            System.out.println(br.readLine());
+        }
+        br.close();
     }
 }
