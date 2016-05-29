5fd599ec1cd38cb0ebb601be67b52a774ae3adf5
Deop <feukorv@gmail.com>
2016-05-29 22:36:56 +0300

    readInputParameters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,24 @@
 package com.company;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        System.out.println("Enter path to txt file");
+        Scanner scanner1 = new Scanner(System.in);
+        String s = scanner1.next();
+        System.out.println("Enter number of lines to read (int value) or send empty line for default value");
+        Scanner scanner2 = new Scanner(System.in);
+        int i = 10;
+        if (!scanner2.nextLine().equals("")) {
+            i = scanner2.nextInt();
+        }
+        System.out.println(s);
+        System.out.println(i);
+        readTestCasesFromFile(s, i);
     }
 
     public static void readTestCasesFromFile(String filePath) throws IOException {
